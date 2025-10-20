bf9d1c1adecd7387b6930ad50ff59aa6b9ea90ed
eng-mwaweza <selemanmwaweza55@gmail.com>
2025-10-20 15:00:02 +0300

    Updated settings

--- globalagency_project/settings.py
+++ globalagency_project/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY='final'
 DEBUG=True
-ALLOWED_HOSTS=[]
+ALLOWED_HOSTS=["*"]
 INSTALLED_APPS=['django.contrib.admin','django.contrib.auth','django.contrib.contenttypes','django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles','crispy_forms','global_agency','employee',]
 MIDDLEWARE=['django.middleware.security.SecurityMiddleware','django.contrib.sessions.middleware.SessionMiddleware','django.middleware.common.CommonMiddleware','django.middleware.csrf.CsrfViewMiddleware','django.contrib.auth.middleware.AuthenticationMiddleware','django.contrib.messages.middleware.MessageMiddleware','django.middleware.clickjacking.XFrameOptionsMiddleware']
 ROOT_URLCONF='globalagency_project.urls'
