631a08c7d7bc2cffd36719a83021573525d49890
eng-mwaweza <selemanmwaweza55@gmail.com>
2025-10-28 13:57:54 +0300

    update template and views

--- global_agency/urls.py
+++ global_agency/urls.py
@@ -10,5 +10,10 @@ urlpatterns = [
     path('start-application/', views.start_application, name='start_application'),
     path('vyuo-vya-ndani/', views.vyuo_ndani, name='vyuo_ndani'),
     path('university/<str:university_name>/', views.university_detail, name='university_detail'),
-]
+   # New URLs for universities
+      path('universities/country/<str:country>/', views.country_universities, name='country_universities'),
+    path('universities/abroad/<str:university_slug>/', views.abroad_university_detail, name='abroad_university_detail'),
+    path('universities/countries/', views.all_countries, name='all_countries'),
+    
+     ]
 
